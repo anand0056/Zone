828cae4afc84663265257f0a2f9467df5cb1857a
satyam Rao <satyampriyam98@gmail.com>
2023-12-19 18:19:37 +0530

    6th commit

--- src/views/View4.jsx
+++ src/views/View4.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ListBox from "../elements/listBox";
+import CardsPalete from "../elements/cardsPalete";
 
 const View4 = () => {
+    const [viewType, setViewType] = useState(false);
+    const handleClick = () => {
+        setViewType(!viewType);
+      };
   return (
     <>
       <h3 className=" font-extrabold text-4xl p-4">Your Orders</h3>
       <div className="border-2 border-black"></div>
       <div className="flex flex-col gap-4 p-4">
-        <ListBox />
-        <ListBox />
-
-        <ListBox />
-        <ListBox />
+      <button onClick={handleClick}>
+        {viewType ? 'ON' : 'OFF'}
+      </button>
+      {viewType ? <ListBox/> : <CardsPalete/>}
       </div>
 
       <div className="border-2 border-black"></div>
